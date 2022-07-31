400528fbd77a94006cd0f8f9fe5704efc656051b
hectorjesuscavi <hectorjesuscavi@gmail.com>
2022-07-31 01:58:49 -0500

    PASO1.- configuracion de archivo properties para la conexion a la base de datos

--- crudapirest/src/main/java/com/mx/crudapi/CrudapirestApplication.java
+++ crudapirest/src/main/java/com/mx/crudapi/CrudapirestApplication.java
@@ -3,9 +3,19 @@ package com.mx.crudapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ *@class_name：CrudapirestApplication
+ *@comments: clase principal para inicializar y ejecutar un proyecto spring
+ *@param: sin parametros
+ *@return: sin parametros
+ *@author: Héctor Camargo
+ *@createtime:31 jul. 2022
+ */
 @SpringBootApplication
 public class CrudapirestApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudapirestApplication.class, args);
 	}
