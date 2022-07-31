935bc38b7820ea54127a37f9a5cafae703e7257b
hectorjesuscavi <hectorjesuscavi@gmail.com>
2022-07-31 01:17:15 -0500

    Proyecto en blanco listo para poder configurar y empezar un proyecto apirest con spring boot

---
+++ crudapirest/src/main/java/com/mx/crudapi/CrudapirestApplication.java
@@ -0,0 +1,13 @@
+package com.mx.crudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudapirestApplication.class, args);
+	}
+
+}
